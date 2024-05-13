547edc4d8cff4fe8d2d99c7b794467e07b55f614
hagghat <mahach0396@gmail.com>
2024-05-13 20:39:09 +0600

    update card

--- src/Components/JobCards/JobCards.jsx
+++ src/Components/JobCards/JobCards.jsx
@@ -1,42 +1,50 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion"; // Import motion from Framer Motion
 
+const JobCards = ({ job }) => {
+  const {
+    category,
+    _id,
+    userName,
+    salaryRange,
+    postingDate,
+    applicationDeadline,
+  } = job || {};
 
-const JobCards = ({job}) => {
-  
-
-  
-  const {category, _id,  userName,salaryRange,postingDate,
-    applicationDeadline } = job || {}
- console.log(job);
-  
-  
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 mb-4">
-      <div className="justify-between items-center mb-2">
-        <h3 className="text-lg font-semibold"></h3>
-        <span className="text-gray-500">Posted by:{userName} </span>
+    <motion.div className="bg-zinc-300 rounded-lg shadow-lg p-4 mb-4" 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }} 
+      transition={{ duration: 0.5 }} 
+    >
+      <div className="">
+        <div className="justify-between items-center mb-2 text-gray-800">
+          <h3 className="text-lg font-semibold"></h3>
+          <span className="text-amber-700 text-md font-semibold">Posted by: {userName} </span>
+        </div>
+        <div className="justify-between items-center mb-2">
+          <h3 className="text-lg font-semibold"></h3>
+          <span className="text-sky-700 text-md font-bold">Job Category: {category} </span>
+        </div>
+        <div className="flex justify-between items-center mb-2">
+          <span className="text-green-700 text-md font-bold">Posting Date: {postingDate} </span>
+          <span className="text-red-700 text-md font-semibold">Deadline: {applicationDeadline} </span>
+        </div>
+        <div className="flex justify-between items-center gap-4 mb-5">
+          <span className="text-blue-800 text-md">Salary Range: {salaryRange}</span>
+          <span className="text-amber-700 text-md">Applicants: {} </span>
+        </div>
       </div>
-      <div className="justify-between items-center mb-2">
-        <h3 className="text-lg font-semibold"></h3>
-        <span className="text-gray-500">Job Category :{category} </span>
-      </div>
-      <div className="flex justify-between items-center mb-2">
-        <span className="text-gray-500">Posting Date: {postingDate} </span>
-        <span className="text-gray-500">Deadline: {applicationDeadline} </span>
-      </div>
-      <div className="flex justify-between items-center gap-4 mb-5">
-        <span className="text-gray-500">Salary Range:{salaryRange}</span>
-        <span className="text-gray-500">Applicants: {} </span>
-      </div>
-       <Link to={`/job/${_id}`}>
 
-       <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">
-        View Details
-      </button>
-       
-       
-       </Link>
-    </div>
+      <Link to={`/job/${_id}`}>
+        <motion.button 
+          className="bg-green-600 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
+          whileHover={{ scale: 1.1 }} // Animation properties on hover
+        >
+          View Details
+        </motion.button>
+      </Link>
+    </motion.div>
   );
 };
 
